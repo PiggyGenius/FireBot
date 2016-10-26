89f90cc0ba5a82d6a6eef1ceab125960cd8c0904
piggygenius <ludo.itrts@gmail.com>
2016-10-26 18:36:50 +0200

    Coordonnee and Carte are kind of done

---
+++ src/Carte.java
@@ -0,0 +1,73 @@
+public class Carte {
+	private int tailleCases;
+	private Case[][] grille_case;
+
+	public Carte(){
+	}
+
+	public void evenement(long date){
+	}
+
+	public int getNbLignes(){
+		return grille_case[0].length;
+	}
+
+	public int getNbColonnes(){
+		return grille_case.length;
+	}
+
+	public int getTailleCases(){
+		return this.tailleCases;
+	}
+
+	public Case getCase(int lig, int col){
+		if(lig < 0 || lig > this.getNbLignes() || col < 0 || col > this.getNbColonnes())
+			throw new IllegalArgumentException();
+		return this.grille_case[lig][col];
+	}
+
+	private Coordonne getCoordonnee(Case src, Direction dir){
+		Coordonnee voisin = new Coordonnee(src.getLigne(),src.getColonne());
+		int x = voisin.getLigne();
+		int y = voisin.getColonne();
+		switch(dir){
+			case NORD:
+				voisin.setLigne(x+1);
+				break;
+			case SUD:
+				voisin.setLigne(x-1);
+				break;
+			case EST:
+				voisin.setColonne(y+1);
+				break;
+			case OUEST:
+				voisin.setColonne(y-1);
+				break;
+			default :
+				System.out.println("dir n'est pas une Direction");
+				throw new IllegalArgumentException();
+				break;
+		}
+		return voisin;
+	}
+
+	public boolean voisinExiste(Case src, Direction dir){
+		Coordonnee voisin = getCoordonnee(src,dir);
+		int x = voisin.getLigne();
+		int y = voisin.getColonne();
+		if(x < 0 || x > getNbLignes() || y < 0 || y > getNbColonnes())
+			return false;
+		return true;
+	}
+
+	public Case getVoisin(Case src, Direction dir){
+		if(voisinExiste(src,dir)){
+			Coordonnee voisin = getCoordonnee(src,dir);
+			return grille_case[voisin.getLigne()][voisin.getColonne];
+		}
+		else {
+			System.out.println("Cette case n'a pas de voisin dans cette direction.");
+			throw new IllegalArgumentException();
+		}
+	}
+}
---
+++ src/Coordonnee.java
@@ -0,0 +1,34 @@
+public class Coordonnee {
+	private int x;
+	private int y;
+
+	public Coordonnee(int x,int y){
+		this.x = x;
+		this.y = y;
+	}
+
+	public int getLigne(){
+		return this.x;
+	}
+
+	public int getColonne(){
+		return this.y;
+	}
+
+	public int setLigne(int x){
+		if(x < 0)
+			throw new IllegalArgumentException();
+		this.x = x;
+	}
+
+	public int setColonne(int y){
+		if(y < 0)
+			throw new IllegalArgumentException();
+		this.y = y;
+	}
+
+	@Override
+	public String toString(){
+		return "("+this.x+";"+this.y+")";
+	}
+}
