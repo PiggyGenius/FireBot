531389568bcfdb499c7a9582cf42031036dc0778
cyrilcarlin <cyrilcarlin@wificampus-037105.grenet.fr>
2016-11-15 08:01:53 +0100

    Classe Matrice terminée

--- src/simulation/Matrice.java
+++ src/simulation/Matrice.java
@@ -1,41 +1,57 @@
 package simulation;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+import chemin.Chemin;
+import chemin.PlusCourtChemin;
 import robot.Robot;
 
 public class Matrice {
 	
-	Simulateur sim;
 	int[][] matrice;
 
-	public Matrice(Simulateur sim) {
-		this.sim = sim;
-		int nbRobot = sim.getSimulation().getNbRobots();
-		int nbIncendie = sim.getSimulation().getNbIncendies();
-		this.matrice = new int[nbRobot][nbIncendie];
+	public Matrice(int nbRobots, int nbIncendies) {
+		this.matrice = new int[nbRobots][nbIncendies];
 	}
 
-	public void init() {
-		for (int i = 0; i < this.sim.getSimulation().getNbRobots(); i++) {
-			for (int j = 0; j < this.sim.getSimulation()
-					.getNbIncendies(); j++) {
-				Robot robotActu = this.sim.getSimulation().getRobot(i);
-				Incendie incendieActu = this.sim.getSimulation().getIncendie(j);
+public void init(ArrayList<Incendie> listeIncendies, ArrayList<Robot> listeRobots, Simulateur sim) {
+		for (int i = 0; i < matrice.length; i++) {
+			for (int j = 0; j < matrice[0].length; j++) {
+				Robot robotActu = listeRobots.get(i);
+				Incendie incendieActu = listeIncendies.get(j);
 				if (robotActu.getCapaciteReservoir() > incendieActu
 						.getIntensite()) {
-					matrice[i][j] = (int) (this.sim.getSimulation()
+					Chemin chemin = sim.getSimulation()
 							.getChemin(robotActu.getPosition(),
 									incendieActu.getCase(),
-									robotActu.getVitesseMap())
-							.getTemps()
-							+ ((float) incendieActu.getIntensite()
-									/ (float) robotActu.getLitresUnitaire())
-									* robotActu.getTempsUnitaire());
+									robotActu.getVitesseMap());
+					if(chemin == null) {
+						matrice[i][j] = Integer.MAX_VALUE;
+					}
+					else {
+						matrice[i][j] = (int) (chemin.getTemps()
+								+ ((float) incendieActu.getIntensite()
+										/ (float) robotActu.getLitresUnitaire())
+										* robotActu.getTempsUnitaire());
+					}
 				} else {
-					matrice[i][j] = (int) (this.sim.getSimulation()
+					Chemin chemin = sim.getSimulation()
 							.getChemin(robotActu.getPosition(),
 									incendieActu.getCase(),
-									robotActu.getVitesseMap())
-							.getTemps() + robotActu.getTempsUnitaire());
+									robotActu.getVitesseMap());
+					if(chemin == null) {
+						matrice[i][j] = Integer.MAX_VALUE;
+					}
+					else {
+						matrice[i][j] = (int) (sim.getSimulation()
+								.getChemin(robotActu.getPosition(),
+										incendieActu.getCase(),
+										robotActu.getVitesseMap())
+								.getTemps() + robotActu.getTempsUnitaire());
+					}
+					
 				}
 			}
 		}
@@ -43,7 +59,7 @@ public class Matrice {
 
 	private int minLigne(int i) {
 		int minActu = Integer.MAX_VALUE;
-		for (int j = 0; j < this.sim.getSimulation().getNbIncendies(); j++) {
+		for (int j = 0; j < matrice[0].length; j++) {
 			if (this.matrice[i][j] < minActu) {
 				minActu = matrice[i][j];
 			}
@@ -52,16 +68,38 @@ public class Matrice {
 	}
 
 	private void soustrLigne(int i, int val) {
-		for (int j = 0; j < this.sim.getSimulation().getNbIncendies(); j++) {
+		for (int j = 0; j < matrice[0].length; j++) {
 			matrice[i][j] -= val;
 		}
 	}
 
-	private void ordonnancementRealisable() {
+	public void ordonnancementRealisable() {
 		int min;
-		for (int i = 0; i < this.sim.getSimulation().getNbRobots(); i++) {
+		for (int i = 0; i < matrice.length; i++) {
 			min = minLigne(i);
 			this.soustrLigne(i, min);
 		}
 	}
+	
+	public int getZero(int ligne) {
+		for(int j = 0; j < matrice[0].length; j++) {
+			if(matrice[ligne][j] == 0) {
+				return j;
+			}
+		}
+		return -1; /* Par les transformations effectuées en amont */
+	}			   /* on est sûr qu'il y a au moins 1 zero dans chaque ligne */
+	
+	
+	public List<Integer> searchZero(int ligne) {
+		List<Integer> l = new LinkedList<Integer>();
+		for(int j = 0; j < matrice[0].length; j++) {
+			if(matrice[ligne][j] == 0) {
+				l.add(j);
+			}
+		}
+		return l;
+	}
+	
+	
 }
