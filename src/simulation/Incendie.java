af2ecf4187c8dd309b7714736c380960d47663fd
Maxime Deloche <maxime.deloche@ensimag.grenoble-inp.fr>
2016-11-15 14:32:27 +0100

    Added a lot of javadoc again

--- src/simulation/Incendie.java
+++ src/simulation/Incendie.java
@@ -19,6 +19,8 @@ public class Incendie {
 		this.enExtinction = false;
 	}
 
+	/** Constructeur d'Incendie
+	 * @param i incendie a copier */
 	public Incendie(Incendie i) {
 		this(i.getCase(), i.getIntensite());
 	}
@@ -41,10 +43,12 @@ public class Incendie {
 		return this.intensite;
 	}
 
+	/** @return true si le feu est en cours d'extinction */
 	public boolean getEnExtinction() {
 		return this.enExtinction;
 	}
 
+	/** @param val true si le feu passe en extinction */
 	public void setEnExtinction(boolean val) {
 		this.enExtinction = val;
 	}
